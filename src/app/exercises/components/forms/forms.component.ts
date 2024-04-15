f97e26b70a4cdded6cbcbfa335eea9dc84734305
conoryo79 <38634819+conoryo79@users.noreply.github.com>
2024-04-15 12:40:51 +0000

    ejercicios 20240415

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClient } from '@angular/common/http'
+import { Component, input } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
@@ -8,4 +9,24 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './forms.component.css',
   imports: [ReactiveFormsModule],
 })
-export class FormsComponent {}
+export class FormsComponent {
+  habitos: { name: string; date: Date }[] = []
+  message: string = ''
+  habitoForm = new FormGroup({
+    habito: new FormControl('', [Validators.required, Validators.min(2)]),
+  })
+
+  onSubmit() {
+    /*  if (this.habitoForm.controls.habito.valid){
+      this.message=''
+        this.habitos.push(this.habitoForm.value.habito??"")
+    }
+*/
+    if (this.habitoForm.invalid) {
+      this.message = 'invalid'
+      return
+    }
+    this.message = ''
+    this.habitos.push(this.habitoForm.value.habito ?? '')
+  }
+}
